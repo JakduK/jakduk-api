ccb078db94d5387f1fe6010ce9489a2acadaa1eb
Pyohwan <phjang1983@gmail.com>
2014-07-20 15:23:10 +0900

    maintain session HTTPS to HTTP.

---
+++ src/com/jakduk/authority/HttpsCookieFilter.java
@@ -0,0 +1,56 @@
+package com.jakduk.authority;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * @author <a href="mailto:phjang1983@daum.net">Jang,Pyohwan</a>
+ * @company  : http://jakduk.com
+ * @date     : 2014. 7. 19.
+ * @desc     :
+ */
+public class HttpsCookieFilter implements Filter {
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response,
+			FilterChain chain) throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		
+		final HttpServletRequest httpRequest = (HttpServletRequest) request;
+		final HttpServletResponse httpResponse = (HttpServletResponse) response;
+		final HttpSession session = httpRequest.getSession(false);
+
+		if (session != null) {
+			final Cookie sessionCookie = new Cookie("JSESSIONID", session.getId());
+			sessionCookie.setMaxAge(-1);
+			sessionCookie.setSecure(false);
+			sessionCookie.setPath(httpRequest.getContextPath());
+			httpResponse.addCookie(sessionCookie);
+		}
+
+		chain.doFilter(request, response);
+	}
+		
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
