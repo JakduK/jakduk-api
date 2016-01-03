a323823e129b969aafe6ed56ddc7f3199a22acbd
Pyohwan <phjang1983@daum.net>
2016-01-03 22:52:49 +0900

    Test RESTful API.

--- jakduk-web/src/com/jakduk/controller/SampleController.java
+++ jakduk-web/src/com/jakduk/controller/SampleController.java
@@ -6,7 +6,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @Controller
@@ -185,10 +187,14 @@ public class SampleController {
 	}
 
 	@RequestMapping(value ="/rest01", method = RequestMethod.POST)
-	public String rest01(@RequestBody Map body) {
+	@ResponseBody
+	public Map rest01(@RequestBody String body) {
 		logger.debug(body);
 
-		return "sample/rest01";
+		Map result = new HashMap<>();
+		result.put("result", Boolean.TRUE);
+
+		return result;
 	}
 
 }
