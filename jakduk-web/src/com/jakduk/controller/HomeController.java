db2920b9d9bb72122ffc8f8f6f640db260c1e35b
Pyohwan <phjang1983@daum.net>
2016-01-04 23:00:51 +0900

    close #21

--- jakduk-web/src/com/jakduk/controller/HomeController.java
+++ jakduk-web/src/com/jakduk/controller/HomeController.java
@@ -10,10 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.LocaleResolver;
 
 import com.jakduk.service.CommonService;
@@ -129,7 +126,7 @@ public class HomeController {
 		
 		Locale locale = localeResolver.resolveLocale(request);
 		String language = commonService.getLanguageCode(locale, lang);
-		
+
 		homeService.getBoardLatest(model);
 		homeService.getUserLatest(model, language);
 		homeService.getGalleryLatest(model);
