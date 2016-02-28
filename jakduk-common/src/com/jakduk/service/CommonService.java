e16095af50042f9b9b5c598fd08a812caed0cd95
silverprize <leesilverprize@gmail.com>
2016-02-28 21:14:41 +0900

    incorrect redirect

--- jakduk-common/src/com/jakduk/service/CommonService.java
+++ jakduk-common/src/com/jakduk/service/CommonService.java
@@ -252,7 +252,7 @@ public class CommonService {
 	public Boolean isRedirectUrl(String url) {
 		Boolean result = true;
 		
-		String[] deny = {"login", "logout", "j_spring", "write", "admin", "resetpassword"};
+		String[] deny = {"login", "logout", "j_spring", "write", "admin", "reset_password", "confirm_password"};
 		
 		for(int idx = 0 ; idx < deny.length ; idx++) {
 			if (url.contains(deny[idx])) {
