dd7a9121303cab974cde02695ebfebc921a18008
Pyohwan <phjang1983@daum.net>
2016-03-31 22:57:22 +0900

    각종 properties 파일 통합.

--- jakduk-common/src/com/jakduk/configuration/SlackAppender.java
+++ jakduk-common/src/com/jakduk/configuration/SlackAppender.java
@@ -1,24 +1,39 @@
-package com.jakduk.common;
+package com.jakduk.configuration;
 
 import ch.qos.logback.classic.Level;
 import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.Layout;
+import ch.qos.logback.core.LayoutBase;
 import ch.qos.logback.core.UnsynchronizedAppenderBase;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import net.gpedro.integrations.slack.SlackApi;
 import net.gpedro.integrations.slack.SlackMessage;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * Created by pyohwan on 16. 3. 30.
  */
+
+@Data
 public class SlackAppender extends UnsynchronizedAppenderBase<ILoggingEvent> {
+
+    private String webhook;
+    private String channel;
+    private String username;
+    private Layout<ILoggingEvent> layout;
 
     @Override
     protected void append(ILoggingEvent iLoggingEvent) {
 
         if (iLoggingEvent.getLevel().isGreaterOrEqual(Level.ERROR)) {
             // Send simple message in different room with custom name
-            SlackApi api = new SlackApi("https://hooks.slack.com/services/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");
-            api.call(new SlackMessage("#jakduk_log", "JAKDUK", iLoggingEvent.getMessage()));
+            SlackApi api = new SlackApi(webhook);
+            api.call(new SlackMessage(channel, username, layout.doLayout(iLoggingEvent)));
         }
-
     }
 }
+
+
