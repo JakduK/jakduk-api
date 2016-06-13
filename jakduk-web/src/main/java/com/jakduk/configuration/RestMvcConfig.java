313b53d5c6b693c755c4424b217c060d28603cee
pyohwan <phjang1983@daum.net>
2016-06-13 23:18:04 +0900

    spring-data-rest 실패.

--- jakduk-web/src/main/java/com/jakduk/configuration/RestMvcConfig.java
+++ jakduk-web/src/main/java/com/jakduk/configuration/RestMvcConfig.java
@@ -3,6 +3,7 @@ package com.jakduk.configuration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
+import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
@@ -12,5 +13,12 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @EnableWebMvc
-public class RestMvcConfig extends RepositoryRestConfigurerAdapter {
+public class RestMvcConfig extends RepositoryRestMvcConfiguration {
+
+    @Override
+    protected void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+        super.configureRepositoryRestConfiguration(config);
+
+        config.setBasePath("/rest");
+    }
 }
