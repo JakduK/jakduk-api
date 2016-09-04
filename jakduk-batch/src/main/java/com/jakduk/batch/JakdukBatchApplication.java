b8875597d9bbae21295c95ed81feecb0f1f2fa54
Pyohwan <phjang1983@daum.net>
2016-09-04 23:07:32 +0900

    jakduk-batch 모듈 추가.

---
+++ jakduk-batch/src/main/java/com/jakduk/batch/JakdukBatchApplication.java
@@ -0,0 +1,15 @@
+package com.jakduk.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+public class JakdukBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JakdukBatchApplication.class, args);
+	}
+}
