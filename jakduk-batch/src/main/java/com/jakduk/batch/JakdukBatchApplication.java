eb40ad485da803ce7c561bca36abf13e4a539ac0
Pyohwan <phjang1983@daum.net>
2016-09-07 00:08:22 +0900

    spring-batch 설정 중.

--- jakduk-batch/src/main/java/com/jakduk/batch/JakdukBatchApplication.java
+++ jakduk-batch/src/main/java/com/jakduk/batch/JakdukBatchApplication.java
@@ -1,7 +1,6 @@
 package com.jakduk.batch;
 
 import com.jakduk.core.configuration.CoreRootConfig;
-import org.springframework.batch.core.configuration.annotation.BatchConfigurer;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
@@ -18,6 +17,5 @@ public class JakdukBatchApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(JakdukBatchApplication.class, args);
 	}
-
 
 }
