b9634d8a394e53d4bf55d8585ae92c285af3856c
Pyohwan <phjang1983@daum.net>
2016-09-22 00:55:59 +0900

    몽고디비 write 안되는 현상 수정.

--- jakduk-core/src/main/java/com/jakduk/core/configuration/MongoConfig.java
+++ jakduk-core/src/main/java/com/jakduk/core/configuration/MongoConfig.java
@@ -2,6 +2,9 @@ package com.jakduk.core.configuration;
 
 import com.mongodb.Mongo;
 import com.mongodb.MongoClient;
+import com.mongodb.ServerAddress;
+import org.apache.commons.lang3.StringUtils;
+import org.elasticsearch.common.transport.InetSocketTransportAddress;
 import org.jongo.Jongo;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,6 +14,15 @@ import org.springframework.data.mongodb.core.MongoClientFactoryBean;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 import javax.annotation.Resource;
+import java.net.InetAddress;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * @author pyohwan
@@ -31,7 +43,19 @@ public class MongoConfig extends AbstractMongoConfiguration {
 
     @Override
     public Mongo mongo() throws Exception {
-        return new MongoClient(environment.getProperty("mongo.host.name"), environment.getProperty("mongo.host.port", Integer.class));
+
+        List<ServerAddress> seeds = Stream.of(StringUtils.split(environment.getProperty("mongo.host.name"), ","))
+                .map(hostName -> {
+                    try {
+                        URL url = new URL(hostName);
+                        return new ServerAddress(url.getHost(), url.getPort());
+                    } catch (MalformedURLException e) {
+                        return null;
+                    }
+                })
+                .collect(Collectors.toList());
+
+        return new MongoClient(seeds);
     }
 
     @Bean
