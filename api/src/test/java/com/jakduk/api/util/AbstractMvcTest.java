08b36769e9f89c3ce0046d37fcd94bbddd84ae71
Pyohwan <phjang1983@daum.net>
2016-10-31 00:52:07 +0900

    ApiInitializer 삭제

--- api/src/test/java/com/jakduk/api/util/AbstractMvcTest.java
+++ api/src/test/java/com/jakduk/api/util/AbstractMvcTest.java
@@ -1,14 +1,10 @@
 package com.jakduk.api.util;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.jakduk.api.configuration.ApiRootConfig;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
