71d6715824930c454062b631c0108818aeb08acc
Pyohwan <phjang1983@daum.net>
2016-11-13 23:08:48 +0900

    crawler4j 연습

---
+++ core/src/test/java/com/jakduk/core/crawler/CrawlerTest.java
@@ -0,0 +1,47 @@
+package com.jakduk.core.crawler;
+
+import edu.uci.ics.crawler4j.crawler.CrawlConfig;
+import edu.uci.ics.crawler4j.crawler.CrawlController;
+import edu.uci.ics.crawler4j.fetcher.PageFetcher;
+import edu.uci.ics.crawler4j.robotstxt.RobotstxtConfig;
+import edu.uci.ics.crawler4j.robotstxt.RobotstxtServer;
+import org.junit.Test;
+
+/**
+ * Created by pyohwan on 16. 11. 13.
+ */
+public class CrawlerTest {
+
+    @Test
+    public void test01() throws Exception {
+        String crawlStorageFolder = "/JakduK/crawl";
+        int numberOfCrawlers = 1;
+
+        CrawlConfig config = new CrawlConfig();
+        config.setMaxDepthOfCrawling(1);
+        config.setCrawlStorageFolder(crawlStorageFolder);
+
+        /*
+         * Instantiate the controller for this crawl.
+         */
+        PageFetcher pageFetcher = new PageFetcher(config);
+        RobotstxtConfig robotstxtConfig = new RobotstxtConfig();
+        RobotstxtServer robotstxtServer = new RobotstxtServer(robotstxtConfig, pageFetcher);
+        CrawlController controller = new CrawlController(config, pageFetcher, robotstxtServer);
+
+        /*
+         * For each crawl, you need to add some seed urls. These are the first
+         * URLs that are fetched and then the crawler starts following links
+         * which are found in these pages
+         */
+        controller.addSeed("http://sports.news.naver.com/kfootball/schedule/index.nhn?category=kleague");
+
+        /*
+         * Start the crawl. This is a blocking operation, meaning that your code
+         * will reach the line after this only when crawling is finished.
+         */
+        controller.start(MyCrawler.class, numberOfCrawlers);
+
+
+    }
+}
