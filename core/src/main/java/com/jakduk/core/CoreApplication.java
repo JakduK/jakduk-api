470473f976494a9b95d54884b4ee2da681a9c02d
Jang,Pyohwan <phjang1983@daum.net>
2017-07-06 23:46:46 +0900

    properties을 yml로 전환

--- core/src/main/java/com/jakduk/core/CoreApplication.java
+++ core/src/main/java/com/jakduk/core/CoreApplication.java
@@ -1,16 +1,13 @@
 package com.jakduk.core;
 
 import net.gpedro.integrations.slack.SlackApi;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.core.env.Environment;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 import javax.annotation.Resource;
 
@@ -20,13 +17,13 @@ import javax.annotation.Resource;
 
 @SpringBootApplication
 @PropertySource("classpath:/application-core.properties")
-@EnableScheduling
 public class CoreApplication {
 
     @Resource
     private Environment environment;
 
     public static void main(String[] args) {
+
         new SpringApplicationBuilder(CoreApplication.class)
                 .web(false)
                 .profiles("core-default")
@@ -48,6 +45,5 @@ public class CoreApplication {
 
         return messageSource;
     }
-
 
 }
