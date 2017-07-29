d15f667b64ece05835bbb69538ad4e371182c624
Jang,Pyohwan <phjang1983@daum.net>
2017-07-29 15:42:08 +0900

    #260 흔적 지우기

--- src/main/java/com/jakduk/api/restcontroller/vo/gallery/SurroundingsGallery.java
+++ src/main/java/com/jakduk/api/restcontroller/vo/gallery/SurroundingsGallery.java
@@ -24,7 +24,4 @@ public class SurroundingsGallery {
     @ApiModelProperty(example = "https://staging.jakduk.com:8080/gallery/thumbnail/58b9050b807d714eaf50a111", value = "사진 썸네일 URL")
     private String thumbnailUrl;
 
-    @ApiModelProperty(example = "10", value = "읽음 수")
-    private int views;
-
 }
