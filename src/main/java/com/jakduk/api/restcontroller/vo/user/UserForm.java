8fc0c1b3ace9ef0fea014d71da57a678863acf7d
Jang,Pyohwan <phjang1983@daum.net>
2017-07-30 01:19:56 +0900

    @WebMvcTest 연습. 근데 이메일 회원 가입은 anonymous 로 가능한데 왜 ROLE_USER를 요구하는지 모르겠다

--- src/main/java/com/jakduk/api/restcontroller/vo/user/UserForm.java
+++ src/main/java/com/jakduk/api/restcontroller/vo/user/UserForm.java
@@ -5,6 +5,7 @@ import com.jakduk.api.common.constraint.ExistUsername;
 import com.jakduk.api.common.constraint.FieldMatch;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
+import lombok.Builder;
 import lombok.Getter;
 import org.hibernate.validator.constraints.Email;
 import org.hibernate.validator.constraints.NotEmpty;
@@ -19,6 +20,7 @@ import javax.validation.constraints.Size;
 @ApiModel(description = "이메일 기반 회원 가입 폼")
 @FieldMatch(first = "password", second = "passwordConfirm", message = "{validation.msg.password.mismatch}")
 @Getter
+@Builder
 public class UserForm {
 
     @ApiModelProperty(required = true, example = "example@jakduk.com")
---
+++ src/test/java/com/jakduk/api/user/UserControllerTests.java
@@ -0,0 +1,73 @@
+package com.jakduk.api.user;
+
+import com.jakduk.api.common.rabbitmq.RabbitMQPublisher;
+import com.jakduk.api.common.util.ObjectMapperUtils;
+import com.jakduk.api.model.db.User;
+import com.jakduk.api.restcontroller.UserRestController;
+import com.jakduk.api.restcontroller.vo.user.UserForm;
+import com.jakduk.api.service.UserService;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * @author pyohwan
+ * 16. 6. 30 오후 11:55
+ */
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(UserRestController.class)
+public class UserControllerTests {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean private AuthenticationManager authenticationManager;
+    @MockBean private UserService userService;
+    @MockBean private PasswordEncoder passwordEncoder;
+    @MockBean private RabbitMQPublisher rabbitMQPublisher;
+    @MockBean private UserDetailsService userDetailsService;
+
+    @Before
+    public void setUp(){
+    }
+
+    @Test
+    @WithMockUser
+    public void createJakdukUser() throws Exception {
+        UserForm form = UserForm.builder()
+                .email("example@jakduk.com")
+                .username("JakdukUser")
+                .password("1111")
+                .passwordConfirm("1111")
+                .build();
+
+        User expectUser = User.builder()
+                .id("abcdef")
+                .email(form.getEmail())
+                .username(form.getUsername())
+                .build();
+
+        given(userService.addJakdukUser(form.getEmail(), form.getUsername(), passwordEncoder.encode(form.getPassword()), null, null, null))
+                .willReturn(expectUser);
+
+        mvc.perform(post("/api/user")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(ObjectMapperUtils.writeValueAsString(form)))
+                .andExpect(status().isOk());
+    }
+}
