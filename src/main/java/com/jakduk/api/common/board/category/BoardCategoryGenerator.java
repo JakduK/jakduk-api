8bb03a4bdbccc79600dc0dd0247be35210c82a95
silverprize <silverprizelee@gmail.com>
2017-09-24 23:48:16 +0900

    refactor 코드리뷰 반영

--- src/main/java/com/jakduk/api/common/board/category/BoardCategoryGenerator.java
+++ src/main/java/com/jakduk/api/common/board/category/BoardCategoryGenerator.java
@@ -1,12 +1,12 @@
 package com.jakduk.api.common.board.category;
 
-import com.google.common.collect.Lists;
 import com.jakduk.api.common.Constants;
 import com.jakduk.api.exception.ServiceError;
 import com.jakduk.api.exception.ServiceException;
 import com.jakduk.api.model.embedded.LocalSimpleName;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Locale;
 import java.util.stream.Collectors;
@@ -14,7 +14,7 @@ import java.util.stream.Stream;
 
 public class BoardCategoryGenerator {
 
-	private BoardCategoryGenerator(){}
+	private BoardCategoryGenerator() {}
 
     public enum Category {
         CLASSIC(Constants.BOARD_TYPE.FOOTBALL, new LocalSimpleName(Locale.US.getLanguage(), "Classic"), new LocalSimpleName(Locale.KOREA.getLanguage(), "클래식")),
@@ -30,7 +30,7 @@ public class BoardCategoryGenerator {
 		IDEA(Constants.BOARD_TYPE.DEVELOPER, new LocalSimpleName(Locale.US.getLanguage(), "Idea"), new LocalSimpleName(Locale.KOREA.getLanguage(), "아이디어")),
 		TIP(Constants.BOARD_TYPE.DEVELOPER, new LocalSimpleName(Locale.US.getLanguage(), "Tip"), new LocalSimpleName(Locale.KOREA.getLanguage(), "팁"));
 
-		private static List<Category> FREE = Lists.newArrayList();
+		private static List<Category> FREE = Collections.emptyList();
 		private static List<Category> FOOTBALL = Stream.of(Category.values()).filter(category -> Constants.BOARD_TYPE.FOOTBALL.equals(category.board)).collect(Collectors.toList());
 		private static List<Category> DEVELOPER = Stream.of(Category.values()).filter(category -> Constants.BOARD_TYPE.DEVELOPER.equals(category.board)).collect(Collectors.toList());
 
@@ -58,36 +58,53 @@ public class BoardCategoryGenerator {
      * 해당 언어에 맞는 게시판 말머리 목록을 가져온다.
      */
     public static List<BoardCategory> getCategories(Constants.BOARD_TYPE boardType, Locale locale) {
-		return Category.list(boardType).stream().map(category -> new BoardCategory(
-				category.name(),
-				category.names.stream().filter(localSimpleName -> locale.getLanguage().equals(localSimpleName.getLanguage()))
-						.collect(Collectors.toList())))
+		return Category.list(boardType)
+				.stream()
+				.map(category -> BoardCategory.builder()
+						.code(category.name())
+						.names(category.names
+								.stream()
+								.filter(localSimpleName -> locale.getLanguage().equals(localSimpleName.getLanguage()))
+								.collect(Collectors.toList()))
+						.build())
 				.collect(Collectors.toList());
 	}
 
     /**
      * 해당 언어에 맞는 게시판 말머리 하나를 가져온다.
      */
-    public static BoardCategory getCategory(Constants.BOARD_TYPE boardType, String targetCategory, Locale locale) {
+    public static BoardCategory getCategory(Constants.BOARD_TYPE boardType, String targetCategoryName, Locale locale) {
 		if (Constants.BOARD_TYPE.FREE.equals(boardType)) {
 			return null;
 		} else {
-			return Category.list(boardType).stream().filter(category -> category.equals(Category.valueOf(targetCategory))).findFirst().map(category -> new BoardCategory(
-					category.name(),
-					category.names.stream().filter(localSimpleName -> locale.getLanguage().equals(localSimpleName.getLanguage()))
-							.collect(Collectors.toList()))
-			).orElseThrow(() -> new ServiceException(ServiceError.NOT_FOUND));
+			Category targetCategory = Category.valueOf(targetCategoryName);
+			return Category.list(boardType)
+					.stream()
+					.filter(category -> category.equals(targetCategory))
+					.findFirst()
+					.map(category -> BoardCategory.builder()
+							.code(category.name())
+							.names(category.names
+									.stream()
+									.filter(localSimpleName -> locale.getLanguage().equals(localSimpleName.getLanguage()))
+									.collect(Collectors.toList()))
+							.build()
+					)
+					.orElseThrow(() -> new ServiceException(ServiceError.NOT_FOUND));
 		}
     }
 
     /**
      * 해당 말머리가 존재 하는지 확인 한다.
      */
-    public static Boolean existCategory(Constants.BOARD_TYPE boardType, String targetCategory) {
+    public static Boolean existCategory(Constants.BOARD_TYPE boardType, String targetCategoryName) {
     	if (Constants.BOARD_TYPE.FREE.equals(boardType)) {
     		return Boolean.TRUE;
 		} else {
-    		return Category.list(boardType).stream().anyMatch(category -> category.equals(Category.valueOf(targetCategory)));
+    		Category targetCategory = Category.valueOf(targetCategoryName);
+    		return Category.list(boardType)
+					.stream()
+					.anyMatch(category -> category.equals(targetCategory));
 		}
     }
 
