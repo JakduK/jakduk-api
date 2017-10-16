9850384b40eef3a2ed7e5bdf352c1c4bdf694a56
Jang,Pyohwan <pyohwan.jang@navercorp.com>
2017-10-16 20:39:51 +0900

    ACL, 해외축구 말머리 추가

--- src/main/java/com/jakduk/api/common/board/category/BoardCategoryGenerator.java
+++ src/main/java/com/jakduk/api/common/board/category/BoardCategoryGenerator.java
@@ -19,10 +19,12 @@ public class BoardCategoryGenerator {
     public enum Category {
         CLASSIC(Constants.BOARD_TYPE.FOOTBALL, new LocalSimpleName(Locale.US.getLanguage(), "Classic"), new LocalSimpleName(Locale.KOREA.getLanguage(), "클래식")),
         CHALLENGE(Constants.BOARD_TYPE.FOOTBALL, new LocalSimpleName(Locale.US.getLanguage(), "Challenge"), new LocalSimpleName(Locale.KOREA.getLanguage(), "챌린지")),
+		ACL(Constants.BOARD_TYPE.FOOTBALL, new LocalSimpleName(Locale.US.getLanguage(), "ACL"), new LocalSimpleName(Locale.KOREA.getLanguage(), "ACL")),
+		NATIONAL_TEAM(Constants.BOARD_TYPE.FOOTBALL, new LocalSimpleName(Locale.US.getLanguage(), "National Team"), new LocalSimpleName(Locale.KOREA.getLanguage(), "국가대표팀")),
+		OVERSEAS_FOOTBALL(Constants.BOARD_TYPE.FOOTBALL, new LocalSimpleName(Locale.US.getLanguage(), "Overseas football"), new LocalSimpleName(Locale.KOREA.getLanguage(), "해외축구")),
         NATIONAL_LEAGUE(Constants.BOARD_TYPE.FOOTBALL, new LocalSimpleName(Locale.US.getLanguage(), "National League"), new LocalSimpleName(Locale.KOREA.getLanguage(), "내셔널리그")),
         K3(Constants.BOARD_TYPE.FOOTBALL, new LocalSimpleName(Locale.US.getLanguage(), "K3"), new LocalSimpleName(Locale.KOREA.getLanguage(), "K3")),
         WK(Constants.BOARD_TYPE.FOOTBALL, new LocalSimpleName(Locale.US.getLanguage(), "WK"), new LocalSimpleName(Locale.KOREA.getLanguage(), "WK")),
-        NATIONAL_TEAM(Constants.BOARD_TYPE.FOOTBALL, new LocalSimpleName(Locale.US.getLanguage(), "National Team"), new LocalSimpleName(Locale.KOREA.getLanguage(), "국가대표팀")),
 
 		QUESTION(Constants.BOARD_TYPE.DEVELOPER, new LocalSimpleName(Locale.US.getLanguage(), "Question"), new LocalSimpleName(Locale.KOREA.getLanguage(), "물음")),
 		JOBS(Constants.BOARD_TYPE.DEVELOPER, new LocalSimpleName(Locale.US.getLanguage(), "Jobs"), new LocalSimpleName(Locale.KOREA.getLanguage(), "일자리")),
