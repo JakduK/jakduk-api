bb2524a59e64e95483206b97dcd27f7afaac04e3
Jang,Pyohwan <phjang1983@daum.net>
2018-06-28 02:33:42 +0900

    #324 WebMvcConfigurerAdapter deprecated 대응

--- src/test/java/com/jakduk/api/TestMvcConfig.java
+++ src/test/java/com/jakduk/api/TestMvcConfig.java
@@ -7,18 +7,17 @@ import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.password.StandardPasswordEncoder;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.mvc.method.annotation.ServletWebArgumentResolverAdapter;
 
 import java.util.List;
 
-public class TestMvcConfig extends WebMvcConfigurerAdapter {
+public class TestMvcConfig implements WebMvcConfigurer {
 
     // org.springframework.mobile.device.Device 구현체를 주입하기 위함
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
         argumentResolvers.add(new ServletWebArgumentResolverAdapter(new DeviceWebArgumentResolver()));
-        super.addArgumentResolvers(argumentResolvers);
     }
 
     @Bean
