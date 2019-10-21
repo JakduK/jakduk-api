76ec3a67f1e9fb761a1e9f7e80160112700c26e1
Jang,Pyohwan <phjang1983@daum.net>
2019-10-22 00:09:54 +0900

    ㅌㅔ스트 코드 추가

--- src/test/java/com/jakduk/api/utils/JakdukUtilsTests.java
+++ src/test/java/com/jakduk/api/utils/JakdukUtilsTests.java
@@ -1,6 +1,7 @@
 package com.jakduk.api.utils;
 
 import com.jakduk.api.common.util.JakdukUtils;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class JakdukUtilsTests {
@@ -8,5 +9,19 @@ public class JakdukUtilsTests {
     @Test
     public void generateTemporaryEmail() {
         System.out.println(JakdukUtils.generateTemporaryEmail());
+    }
+
+    @Test
+    public void stripHtmlTag() {
+        String emojiTag = "<p>\uD83C\uDFC3\u200D♀️\uD83C\uDFC3\u200D♂️\uD83D\uDEB4\u200D♀️\uD83D\uDEB4\u200D♂️</p>";
+        String imgTag = "<img src=\"smiley.gif\" alt=\"Smiley face\" height=\"42\" width=\"42\">️";
+        String aTag = "<a href=\"https://www.w3schools.com\">Visit W3Schools.com!</a>️";
+
+        Assert.assertEquals(27, emojiTag.length());
+        Assert.assertEquals("\uD83C\uDFC3♀️\uD83C\uDFC3♂️\uD83D\uDEB4♀️\uD83D\uDEB4♂️", JakdukUtils.stripHtmlTag(emojiTag));
+        Assert.assertEquals(27, JakdukUtils.stripHtmlTag(emojiTag).length());
+
+        Assert.assertEquals("", JakdukUtils.stripHtmlTag(imgTag));
+        Assert.assertEquals("Visit W3Schools.com!", JakdukUtils.stripHtmlTag(aTag));
     }
 }
